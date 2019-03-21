5ca534a9a04ddd666726a4b97768ca80c77a86fc
Perla Ramos <pvramosr@gmail.com>
2019-03-21 17:31:57 -0600

    Update of the website

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,7 @@
 import * as firebase from 'firebase'
 
 const FirebaseConfig = {
-    apiKey: "AIzaSyAdzkgtR1ht4eDlkzKwvQG4w4M5UlPkcOg",
-    authDomain: "bonni-to.firebaseapp.com",
-    databaseURL: "https://bonni-to.firebaseio.com",
-    projectId: "bonni-to",
-    storageBucket: "bonni-to.appspot.com",
-    messagingSenderId: "48089528505"
+
 };
 
 
